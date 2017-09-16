0d2f2a0aa962b3f650df3c01a313fb0eb0bdd9ed
prathyusha199609 <32007794+prathyusha199609@users.noreply.github.com>
2017-09-16 12:45:19 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		StudentGroup stg = new StudentGroup();
+		stg.StudentGroup(3);
+		Student(569, Tom cruise, 1996-03-03, 50);
+
+
+	}
+
+}
